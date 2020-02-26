ca5757f30dffeffdc6f022a6b4f37fec50aad586
Sam Sharpe <samsharpe@Powerbook-G4.local>
2020-02-25 19:57:53 -0500

    added calorie calculator

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-const salutation = "Hello, World!";
+$("body").on("keydown", ".fruit input", function(e){
+  const current_input = e.target;
+  const $current_input = $(e.target);
 
-alert(salutation);
-console.log(salutation);
+  let calories = $current_input.data("calories");
+
+  calories = parseInt(calories);
+
+  let user_count = $current_input.val();
+
+  user_count = parseFloat(user_count);
+
+  const answer = user_count * calories;
+
+  console.log(answer);
+});
